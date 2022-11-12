b58024753704897f937213533a58f9778a3f56f1
Kut Zhang <kutzhang@gmail.com>
2022-11-13 00:06:55 +0800

    type alias

--- ch02/type_alias.go
+++ ch02/type_alias.go
@@ -1,5 +1,9 @@
 package ch02
 
-func TypeAlias() {
+import "fmt"
 
+func TypeAlias() {
+  type Moon = string
+  var message Moon = "show me the money"
+  fmt.Printf("message: %s\n", message)
 }
--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 
 
 func main()  {
-  ch02.UsingConst()
+  ch02.TypeAlias()
 }
 
