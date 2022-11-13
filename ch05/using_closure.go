21f1826e1a8fc34ca1bbd6b1e81ef05812be7f6e
Kut Zhang <kutzhang@gmail.com>
2022-11-14 00:37:55 +0800

    closure simply use case

--- ch05/using_closure.go
+++ ch05/using_closure.go
@@ -6,6 +6,19 @@ import (
 )
 
 func UsingClosure() {
+	simplyUse()
+	fmt.Println("-------------------------------------------------------------")
+	complexUse()
+}
+
+func buildIdGenerator(prefix string) func() string {
+	return func() string {
+		var subfix = uuid.New()
+		return fmt.Sprintf("%s-%s", prefix, subfix.String())
+	}
+}
+
+func complexUse() {
 	generator := buildIdGenerator("MDC")
 	id := generator()
 	fmt.Printf("id: %s\n", id)
@@ -14,9 +27,14 @@ func UsingClosure() {
 	fmt.Printf("id: %s\n", id)
 }
 
-func buildIdGenerator(prefix string) func() string {
-	return func() string {
-		var subfix = uuid.New()
-		return fmt.Sprintf("%s-%s", prefix, subfix.String())
+func simplyUse() {
+	var number int32 = 32
+	fmt.Printf("Before number changed, number is %d\n", number)
+
+	var changeNumber = func() {
+		number = 64
 	}
+
+	changeNumber()
+	fmt.Printf("After number changed, number is %d\n", number)
 }
