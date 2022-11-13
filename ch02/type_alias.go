74b94aced3b2af500c8d9c6436ed8b0ee1fda07f
Kut Zhang <kutzhang@gmail.com>
2022-11-13 08:32:17 +0800

    study type alias and define new type

--- ch02/type_alias.go
+++ ch02/type_alias.go
@@ -3,7 +3,17 @@ package ch02
 import "fmt"
 
 func TypeAlias() {
-  type Moon = string
-  var message Moon = "show me the money"
-  fmt.Printf("message: %s\n", message)
+  type MooX = string
+  type MooY string
+  var info MooX = "show me the money"
+  var error MooY = "how do you turn this on"
+
+  // will print `string`
+  fmt.Printf("type of info: %T\n", info)
+
+  // will print `ch02.MooY`
+  fmt.Printf("type of error: %T\n", error)
+
+  fmt.Printf("info: %s\n", info)
+  fmt.Printf("error: %s\n", error)
 }
